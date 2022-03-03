5eadabfd154d32f933275ebf82d60e1147a9697b
Adam.Carter <adamkristopher@gmail.com>
2022-03-03 14:00:58 -0600

    style single store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import coffeeStoresData from '../../data/coffee-stores'
 import Head from 'next/head'
+import styles from './coffee-store.module.css'
+import Image from 'next/image'
+import cls from 'classnames'
 
 // getStaticProps also allows us to access params.
 // we can use getStaticProps({params}) or getStaticProps(props) then extract the params with
@@ -43,19 +46,85 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>
   }
 
-  const { address, name, neighbourhood } = props.coffeeStore;
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {
+    console.log('upVote button clicked');
+  }
 
   return (
     <div>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href={'/'}><a>Back to home</a></Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href={'/'}><a>Back to home</a></Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImage}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls('glass', styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" height={24} width={24} />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" height={24} width={24} />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" height={24} width={24} />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button 
+            className={styles.upvoteButton}
+            onClick={handleUpvoteButton}>
+            Up Vote
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default CoffeeStore
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
