171f89c2a60f7881279c5b55c85ba0305b3d2927
Adam.Carter <adamkristopher@gmail.com>
2022-03-08 14:37:29 -0600

    connect Airtable db and build createCoffeeStore internal api

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,53 @@
+import { table, getRecords } from '../../lib/airtable';
+
+const createCoffeeStore = async (req, res) => {
+    if (req.method === 'POST') {
+        const { id, name, address, dma, voting, imgUrl } = req.body;
+
+        try {
+            if (id) {
+                // Find a record.
+                const findCoffeeStoreRecords = await table.select({
+                    filterByFormula: `id=${id}`
+                }).firstPage();
+
+                if (findCoffeeStoreRecords.length !== 0) {
+                    const records = getRecords(findCoffeeStoreRecords);
+                    res.json(records);
+                } else {
+                    // Create a record.
+                    if (name) {
+                        const createRecords = await table.create([
+                            {
+                                fields: {
+                                    id,
+                                    name,
+                                    address,
+                                    dma,
+                                    voting,
+                                    imgUrl
+                                }
+                            }
+                        ]);
+                        const records = getRecords(createRecords);
+                        res.json({ records });
+                    } else {
+                        // The server cannot or will not process the request due to an apparent client error 
+                        // (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing)
+                        res.status(400);
+                        res.json({ message: 'Id or name is missing.' });
+                    }
+                }
+            } else {
+                res.status(400);
+                res.json({ message: 'Id is missing.' });
+            }
+        } catch (err) {
+            console.error('Error Creating or Finding Store', err);
+            res.status(500);
+            res.json({ message: "Error Creating or Finding Store", err });
+        }
+    }
+}
+
+export default createCoffeeStore;
